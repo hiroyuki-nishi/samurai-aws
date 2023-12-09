3f3c1fb3236ad2cf79904cb8e9175e2d1dd8261d
nishi_hiroyuki <nishi_hiroyuki@cyberagent.co.jp>
2023-12-09 11:51:37 +0900

    frontend: tailwind

--- frontend/src/login/Login.tsx
+++ frontend/src/login/Login.tsx
@@ -11,7 +11,12 @@ export const Login: () => JSX.Element = () => {
 
   return (
     <>
-      <div>a</div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Button
+      </button>
     </>
   );
 }
