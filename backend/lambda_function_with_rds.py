31a8ef75e98908dbabf074f9dcd389e186518b9a
nishi_hiroyuki <nishi_hiroyuki@cyberagent.co.jp>
2024-08-03 19:25:54 +0900

    add rds sample

--- backend/lambda_function_with_rds.py
+++ backend/lambda_function_with_rds.py
@@ -13,8 +13,7 @@ db_name = os.environ['DB_NAME']
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# create the database connection outside of the handler to allow connections to be
-# re-used by subsequent function invocations.
+
 try:
     conn = pymysql.connect(host=rds_host, user=user_name, passwd=password, db=db_name, connect_timeout=5)
 except pymysql.MySQLError as e:
@@ -25,26 +24,23 @@ except pymysql.MySQLError as e:
 logger.info("SUCCESS: Connection to RDS for MySQL instance succeeded")
 
 def lambda_handler(event, context):
-    """
-    This function creates a new RDS database table and writes records to it
-    """
-    message = event['Records'][0]['body']
-    data = json.loads(message)
-    CustID = data['CustID']
-    Name = data['Name']
-
     item_count = 0
-    sql_string = f"insert into Customer (CustID, Name) values(%s, %s)"
+    names = []
+    # sql_string = f"insert into Customer (CustID, Name) values(%s, %s)"
 
     with conn.cursor() as cur:
-        cur.execute("create table if not exists Customer ( CustID  int NOT NULL, Name varchar(255) NOT NULL, PRIMARY KEY (CustID))")
-        cur.execute(sql_string, (CustID, Name))
-        conn.commit()
-        cur.execute("select * from Customer")
+        cur.execute("select * from users")
         logger.info("The following items have been added to the database:")
         for row in cur:
             item_count += 1
             logger.info(row)
+            logger.info(row[1])
+            names.append(row[1])
     conn.commit()
-
-    return "Added %d items to RDS for MySQL table" % (item_count)
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": ','.join(names)
+    }
