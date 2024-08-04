f4d045c43748e53c754798f86a1dca030f7f59e3
hiroyuki-nishi <tcka46240@gmail.com>
2024-08-04 13:01:06 +0900

    Java

---
+++ SampleLambda/README.md
@@ -0,0 +1,5 @@
+.zip または JAR ファイルアーカイブで Java Lambda 関数をデプロイする
+https://docs.aws.amazon.com/ja_jp/lambda/latest/dg/java-package.html
+
+Sample Code
+https://github.com/awsdocs/aws-doc-sdk-examples/tree/main/javav2/usecases/creating_lambda_apigateway/src/main/java/com/aws/example
---
+++ SampleLambda/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+/**
+ * This is the entry point for the Lambda function.
+ */
+
+public class Main implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.setStatusCode(200);
+        response.setBody("Hello");
+        return response;
+    }
+}
